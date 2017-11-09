61212cc3ecde85ac022ec8d67050b5a4f1292131
Marcos Rachid <marcos.rachid@matera.com>
2017-11-09 18:07:39 -0200

    stub for other unit tests

--- test/unit/controllers/sites_spec.js
+++ test/unit/controllers/sites_spec.js
@@ -22,22 +22,24 @@ describe('Controller: Site', () => {
         });
     });
 
-    it('should return 400 when an error occurs', () => {
-      const request = {};
-      const response = {
-        send: sinon.spy(),
-        status: sinon.stub()
-      };
-      response.status.withArgs(400).returns(response);
-      Site.find = sinon.stub();
-      Site.find.withArgs({}).rejects({ message: 'Error' });
+    context('when an error occurs', () => {
+      it('should return 400', () => {
+        const request = {};
+        const response = {
+          send: sinon.spy(),
+          status: sinon.stub()
+        };
+        response.status.withArgs(400).returns(response);
+        Site.find = sinon.stub();
+        Site.find.withArgs({}).rejects({ message: 'Error' });
 
-      const siteController = new SiteController(Site);
+        const siteController = new SiteController(Site);
 
-      return siteController.get(request, response)
-        .then(() => {
-          sinon.assert.calledWith(response.send, 'Error');
-        });
+        return siteController.get(request, response)
+          .then(() => {
+            sinon.assert.calledWith(response.send, 'Error');
+          });
+      });
     });
   });
 
@@ -64,40 +66,66 @@ describe('Controller: Site', () => {
         });
     });
 
-    it('should return 400 when an error occurs', () => {
-      const mockId = 'anyId';
-      const request = {
-        params: {
-          id: mockId
-        }
-      };
-      const response = {
-        send: sinon.spy(),
-        status: sinon.stub()
-      };
+    context('when an error occurs', () => {
+      it('should return 400', () => {
+        const mockId = 'anyId';
+        const request = {
+          params: {
+            id: mockId
+          }
+        };
+        const response = {
+          send: sinon.spy(),
+          status: sinon.stub()
+        };
 
-      response.status.withArgs(400).returns(response);
-      Site.find = sinon.stub();
-      Site.find.withArgs({ _id: mockId }).rejects({ message: 'Error' });
+        response.status.withArgs(400).returns(response);
+        Site.find = sinon.stub();
+        Site.find.withArgs({ _id: mockId }).rejects({ message: 'Error' });
 
-      const siteController = new SiteController(Site);
+        const siteController = new SiteController(Site);
 
-      return siteController.getById(request, response)
-        .then(() => {
-          sinon.assert.calledWith(response.send, 'Error');
-        });
+        return siteController.getById(request, response)
+          .then(() => {
+            sinon.assert.calledWith(response.send, 'Error');
+          });
+      });
     });
   });
 
   describe('create() site', () => {
+    it('should call send with a new site', () => {
 
+    });
+
+    context('when an error occurs', () => {
+      it('should return 422', () => {
+
+      });
+    });
   });
 
   describe('update() site', () => {
+    it('should respond with 200 when the site has been updated', () => {
 
+    });
+
+    context('when an error occurs', () => {
+      it('should return 422', () => {
+
+      });
+    });
   });
 
   describe('remove() site', () => {
+    it('should respond with 204 when the site has been deleted', () => {
 
+    });
+
+    context('when an error occurs', () => {
+      it('should return 400', () => {
+
+      });
+    });
   });
 });
