e717ca209f99045ea3ecef3a61e9d1e5e0da296b
Marcos Rachid <marcosrachid@gmail.com>
2017-11-10 02:23:44 -0200

    update unit helper

--- test/unit/controllers/sites_spec.js
+++ test/unit/controllers/sites_spec.js
@@ -3,8 +3,10 @@ import sinon from 'sinon';
 import Site from '../../../src/models/site';
 
 describe('Controller: Site', () => {
-  const defaultSite = [{ "_id" : '5a011039a27e510f9c6ef726', "url" : "http://www.marcosrachid.com.br", "description" : "portifolio 4", "__v" : 0 }];
+  const defaultSite = { "_id" : '5a011039a27e510f9c6ef726', "url" : "http://www.marcosrachid.com.br", "description" : "portifolio 4", "__v" : 0 };
   const defaultRequest = { params: {} };
+  const mockSite = { "url" : "http://www.marcosrachid.com.br", "description" : "portifolio 4" };
+  const expectedSite = { "_id" : '5a011039a27e510f9c6ef726', "url" : "http://www.marcosrachid.com.br", "description" : "portifolio 4", "__v" : 0 };
 
   describe('get() sites', () => {
     it('should call send with a list of sites', () => {
@@ -12,18 +14,18 @@ describe('Controller: Site', () => {
         send: sinon.spy()
       };
       Site.find = sinon.stub();
-      Site.find.withArgs({}).resolves(defaultSite);
+      Site.find.withArgs({}).resolves([defaultSite]);
 
       const siteController = new SiteController(Site);
 
       return siteController.get(defaultRequest, response)
         .then(() => {
-          sinon.assert.calledWith(response.send, defaultSite);
+          sinon.assert.calledWith(response.send, [defaultSite]);
         });
     });
 
     context('when an error occurs', () => {
-      it('should return 400', () => {
+      it('should return 400', done => {
         const request = {};
         const response = {
           send: sinon.spy(),
@@ -44,7 +46,7 @@ describe('Controller: Site', () => {
   });
 
   describe('getById() site', () => {
-    it('should call send with one site', () => {
+    it('should call send with one site', done => {
       const mockId = 'anyId';
       const request = {
         params: {
@@ -56,18 +58,18 @@ describe('Controller: Site', () => {
       };
 
       Site.find = sinon.stub();
-      Site.find.withArgs({ _id: mockId }).resolves(defaultSite);
+      Site.find.withArgs({ _id: mockId }).resolves([defaultSite]);
 
       const siteController = new SiteController(Site);
 
       return siteController.getById(request, response)
         .then(() => {
-          sinon.assert.calledWith(response.send, defaultSite);
+          sinon.assert.calledWith(response.send, [defaultSite]);
         });
     });
 
     context('when an error occurs', () => {
-      it('should return 400', () => {
+      it('should return 400', done => {
         const mockId = 'anyId';
         const request = {
           params: {
@@ -94,36 +96,35 @@ describe('Controller: Site', () => {
   });
 
   describe('create() site', () => {
-    it('should call send with a new site', () => {
-
+    it('should call send with a new site', done => {
     });
 
     context('when an error occurs', () => {
-      it('should return 422', () => {
+      it('should return 422', done => {
 
       });
     });
   });
 
   describe('update() site', () => {
-    it('should respond with 200 when the site has been updated', () => {
+    it('should respond with 200 when the site has been updated', done => {
 
     });
 
     context('when an error occurs', () => {
-      it('should return 422', () => {
+      it('should return 422', done => {
 
       });
     });
   });
 
   describe('remove() site', () => {
-    it('should respond with 204 when the site has been deleted', () => {
+    it('should respond with 204 when the site has been deleted', done => {
 
     });
 
     context('when an error occurs', () => {
-      it('should return 400', () => {
+      it('should return 400', done => {
 
       });
     });
--- test/unit/helpers.js
+++ test/unit/helpers.js
@@ -1,3 +1,6 @@
 import chai from 'chai';
+import sinon from 'sinon';
+import 'sinon-mongoose';
 
 global.expect = chai.expect;
+global.sinon = sinon;
